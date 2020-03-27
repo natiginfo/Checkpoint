ce78a582352107714605a2a6dfd8cc46b4f7d538
Natig Babayev <natig.nizami@gmail.com>
2020-03-27 12:54:30 +0200

    Added docs for Rule and callback

--- src/main/kotlin/com/natigbabayev/checkpoint/core/DefaultRule.kt
+++ src/main/kotlin/com/natigbabayev/checkpoint/core/DefaultRule.kt
@@ -1,11 +1,11 @@
 package com.natigbabayev.checkpoint.core
 
-abstract class DefaultRule<INPUT>(sideEffect: ((INPUT) -> Unit)?) :
-    Rule<INPUT, Boolean>(sideEffect) {
+abstract class DefaultRule<INPUT>(callback: Callback<INPUT>?) :
+    Rule<INPUT, Boolean>(callback) {
 
     override fun canPass(input: INPUT): Boolean {
         val isValid = isValid(input)
-        invokeSideEffect(input, isValid)
+        invokeCallback(input, isValid)
         return isValid
     }
 }
--- src/main/kotlin/com/natigbabayev/checkpoint/core/Rule.kt
+++ src/main/kotlin/com/natigbabayev/checkpoint/core/Rule.kt
@@ -1,12 +1,37 @@
 package com.natigbabayev.checkpoint.core
 
-abstract class Rule<INPUT, OUTPUT>(private val sideEffect: ((INPUT) -> Unit)?) {
+abstract class Rule<INPUT, OUTPUT> @JvmOverloads constructor(private val callback: Callback<INPUT>?) {
 
-    protected abstract fun isValid(input: INPUT): OUTPUT
-
-    protected fun invokeSideEffect(input: INPUT, isValid: Boolean) {
-        if (!isValid) sideEffect?.invoke(input)
+    /**
+     * @param [INPUT] should be same as input of [Rule]
+     */
+    interface Callback<INPUT> {
+        /**
+         * This function must be invoked when [Rule.isValid] returns false.
+         */
+        fun whenInvalid(input: INPUT)
     }
 
+    /**
+     * Function used to define whether a condition is satisfied with given input.
+     * Typically, this function should be invoked from [Rule.canPass].
+     */
+    protected abstract fun isValid(input: INPUT): OUTPUT
+
+    /**
+     * When [isValid] is false, this function will invoke [Callback.whenInvalid].
+     * Typically, this function should be invoked from [Rule.canPass], after result of [Rule.isValid] is ready.
+     *
+     * @param input - input which used for validation
+     * @param isValid - result of [Rule.isValid]
+     */
+    protected fun invokeCallback(input: INPUT, isValid: Boolean) {
+        if (!isValid) callback?.whenInvalid(input)
+    }
+
+    /**
+     * Main entry point of class. This function should invoke [Rule.isValid] with given input
+     * and pass result to [Rule.invokeCallback].
+     */
     abstract fun canPass(input: INPUT): OUTPUT
 }
