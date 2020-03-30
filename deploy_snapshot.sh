798098c57735eb59f905c9e78e2133b58530ab5e
Natig Babayev <natig.nizami@gmail.com>
2020-03-31 00:08:22 +0300

    chore: moved gradle execution to deploy_snapshot.sh

--- deploy_snapshot.sh
+++ deploy_snapshot.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-git rev-list --count HEAD
+commit_count=$(git rev-list --count HEAD)
+
+./gradlew -Pversion="0.1.0-${commit_count}-SNAPSHOT" -PossrhUsername="${{ secrets.OSSRH_USERNAME }}" \
+    -PossrhPassword="${{ secrets.OSSRH_PASSWORD }}"  -PsigningKey="${{ secrets.GPG_KEY_CONTENTS }}" \
+    -PsigningPassword="${{ secrets.GPG_KEY_PASSWORD }}" publishAllPublicationsToMavenCentralRepository
