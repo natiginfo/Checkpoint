a3181c898e3bd46bdf258d562dbccdb4921284dc
Natig Babayev <natig.nizami@gmail.com>
2020-03-31 11:19:27 +0300

    chore: updated shell script

--- deploy_snapshot.sh
+++ deploy_snapshot.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 commit_count=$(git rev-list --count HEAD)
+ossrh_username=$1
+ossrh_password=$2
+gpg_key_contents=$3
+gpg_key_password=$4
 
-./gradlew -Pversion="0.1.0-${commit_count}-SNAPSHOT" -PossrhUsername="${secrets.OSSRH_USERNAME}" \
-    -PossrhPassword="${secrets.OSSRH_PASSWORD}"  -PsigningKey="${secrets.GPG_KEY_CONTENTS}" \
-    -PsigningPassword="${secrets.GPG_KEY_PASSWORD}" publishAllPublicationsToMavenCentralRepository
+./gradlew -Pversion="0.1.0-${commit_count}-SNAPSHOT" -PossrhUsername="${ossrh_username}" \
+    -PossrhPassword="${ossrh_password}"  -PsigningKey="${gpg_key_contents}" \
+    -PsigningPassword="${gpg_key_password}" publishAllPublicationsToMavenCentralRepository
