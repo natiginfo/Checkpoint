af45e32207ef60a1b772dd3857943d9fa5c829e0
Natig Babayev <natig.nizami@gmail.com>
2020-04-03 20:36:15 +0300

    chore: moved publish_to_maven to .scripts folder and now commit_count is equal to commit count since last tag

--- .scripts/publish_to_maven.sh
+++ .scripts/publish_to_maven.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-commit_count=$(git rev-list --count HEAD)
+# If last tag is defined, gets commit count since that tag. Otherwise, gets total commit count
+if latest_tag=$(git describe --abbrev=0 --tags); then
+  commit_count=$(git rev-list --count "${latest_tag}"..HEAD)
+else
+  commit_count=$(git rev-list --count HEAD)
+fi
 
+# Get latest tag and if it's not defined, version will be 0.1.0
 if latest_tag=$(git describe --abbrev=0 --tags); then
   version="${latest_tag}"
 else
   version="0.1.0"
 fi
 
+# If it's snapshot build, version will be
 if [ "$1" = "SNAPSHOT" ]; then
   final_version="${version}-${commit_count}-SNAPSHOT"
 else
