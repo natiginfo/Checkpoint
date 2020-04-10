932522d81b67c83ffee4bc9b6329ad8fbfda380f
Natig Babayev <natig.nizami@gmail.com>
2020-04-10 18:13:35 +0300

    chore: created core abstraction module

---
+++ checkpoint-core-abstraction/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.41"
+    id("org.jetbrains.dokka") version "0.10.0"
+    `maven-publish`
+    signing
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+apply(from = "../gradle/gradle-test-dependencies.gradle.kts")
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+// Configure existing Dokka task to output HTML to typical Javadoc directory
+tasks.dokka {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+// Create dokka Jar task from dokka task output
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    archiveClassifier.set("javadoc")
+    from(tasks.dokka)
+}
+
+// Create sources Jar from main kotlin sources
+val sourcesJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles sources JAR"
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+apply(from = "../gradle/gradle-mvn-push.gradle.kts")
